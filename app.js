1acab8b027d4c2b039a3bfb6dfc079d4b234fc0b
cmapereira <cmapereira@gmail.com>
2023-02-19 13:49:12 -0300

    ajusting app.js

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ var search = document.querySelector('#search');
 var div = document.querySelector('.news');
 var selectCountry = document.querySelector('#country');
 var selectCategory = document.querySelector('#category');
-var arrayNews = [];
-var template = '';
 
 function selectCountryCategory(){
     var arrayNews = [];
-    var country = selectCountry.options[selectCountry.selectedIndex].value
-    var category = selectCategory.options[selectCategory.selectedIndex].value
-    console.log(country, category)
+    var country = selectCountry.options[selectCountry.selectedIndex].value;
+    var category = selectCategory.options[selectCategory.selectedIndex].value;
+    console.log(country, category);
 
     var url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=3abf309cbb9b4d6fbd691c5fd1ec6455`;
 
     var req = new Request(url);
     fetch(req)
         .then((response) => {
-            return response.json()
+            return response.json();
     }).then((data) => {
         arrayNews = data.articles
         console.log(arrayNews)
@@ -35,10 +33,9 @@ function selectCountryCategory(){
                         <br>
                         <hr> 
                         `
-            div.innerHTML += template;
+            div.innerHTML += template
         });
     });
-    
 };
 
 function cleardiv(){
